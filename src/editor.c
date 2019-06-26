e0472be073735859f2d26504a85b8f997e6ebd22
JoanDeulofeu <jgehin@student.42.fr>
2019-06-26 19:10:10 +0200

    J: Probleme de ref dans editeur reglé 💪

--- src/editor.c
+++ src/editor.c
@@ -1,24 +1,34 @@
 #include "doom.h"
 
+int		ft_find_ref(t_editor *edi, int decal)
+{
+	if (decal >= 0 && decal < edi->space)
+		return (0);
+	else if (decal > 0)
+		return ((decal / edi->space) * -1);
+	else if (decal < 0)
+		return ((decal / (edi->space +1) -1) * -1);
+	return (0);
+}
+
 void	ft_draw_editor(t_editor *edi, t_texture *tex)
 {
 	t_pos	coord;
-	Uint32	color = 0xaca7a7FF; // #aca7a7
+	Uint32	color = 0xaca7a7FF; //JE SUIS TROP FORT 💪 💪 💪 💪 💪
 
 	// decal_x = decalage total en pixels entre le point 0,0 et le premier point dans l'ecran.
 	// coord.x  decalage en pixels entre le pixel 0 de l'ecran et le premier point de la grille affichée a l'ecran.
 	coord.x = edi->decal_x % edi->space;
 	coord.y = edi->decal_y % edi->space;
-
-	if (edi->decal_x >= 0 && ft_abs(edi->decal_x) < edi->space){
-		edi->ref.x = 0;printf("etat1\n");}
-	else if (edi->decal_x > 0){
-		edi->ref.x = (edi->decal_x / edi->space);printf("etat2\n");}
-	else if (edi->decal_x < 0){
-		edi->ref.x = (edi->decal_x / edi->space);printf("etat3\n");}
-
-
-	printf("coord.x = %d\ndecal_x = %d\nref.x = %d\n\n\n",coord.x , edi->decal_x, edi->ref.x);
+	if (coord.x < 0)
+		coord.x += 20;
+	if (coord.y < 0)
+		coord.y += 20;
+	edi->ref.x = ft_find_ref(edi, edi->decal_x);
+	edi->ref.y = ft_find_ref(edi, edi->decal_y);
+	// printf("coord.x = %d\ndecal_x = %d\nref.x = %d\n\n",coord.x , edi->decal_x, edi->ref.x);
+	// printf("coord.y = %d\ndecal_y = %d\nref.y = %d\n\n\n",coord.y , edi->decal_y, edi->ref.y);
+	printf("REF   X %d | Y %d\n",edi->ref.x, edi->ref.y);
 	while (coord.y < HEIGHT)
 	{
 		while (coord.x < WIDTH)
