09380e9be29622b1d10380dc20bfb674160fac89
Levaillant Clement <clemlvt42@gmail.com>
2019-07-04 11:06:44 +0200

    C: Deplacement presque bon juste petit probleme encore

--- src/mode_deplacement.c
+++ src/mode_deplacement.c
@@ -7,9 +7,8 @@ void mouse_grid(t_main *s, t_pos save)
 
 	diffx = s->ft_mouse.x - save.x;
 	diffy = s->ft_mouse.y - save.y;
-	s->editor->decal_x += diffx;
-	s->editor->decal_y += diffy;
+	s->editor->decal_x = diffx;
+	s->editor->decal_y = diffy;
 	// printf("souris X = %f et souris Y = %f\n", s->ft_mouse.x, s->ft_mouse.y);
 	// printf("souris X = %f et souris Y = %f\n", s->ft_mouse.x, s->ft_mouse.y);
-
 }
