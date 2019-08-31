2ea6254580835dc047b4ba9285bd9a02de934c4d
ydonse <ydonse@ydonses-iMac.local>
2019-08-31 02:35:02 -0700

    merge

--- src/game/visu_joan_drawing.c
+++ src/game/visu_joan_drawing.c
@@ -249,18 +249,39 @@ t_int	*draw_mid_walls(t_main *s, t_int *vtx, t_visu *vs)
 	t_visu	fake_vs;
 	t_dpos	fake_player;
 	double	fake_angle;
+	double	angle_right;
+	double	angle_left;
+	double	demi_fov;
+	t_int 	*vtx_ori;
+	int 	i;
 
+	vtx_ori = vtx;
+	i = 0;
 	while (vtx->ptr->id != vs->end_wall_id)
 	{
-		if (vtx->wall_value != -1)
-		{
-			fake_angle = ft_get_fake_angle(s, vs->player, vtx);
-			fake_player = ft_get_fake_player(s, vs->player, fake_angle, vtx);
-			fake_vs = ft_place_view_plan(s, fake_player, fake_angle, 0x4bd9ffff);
-		}
-
-		else
-			ft_create_new_wall(s, vtx, vs);
+		// if (vtx->wall_value != -1)
+		// {
+		// 	fake_angle = 0;
+		// 	fake_player = ft_get_fake_player(s, vs->player, vtx, &fake_angle);
+		// 	printf("player angle = %f, fake angle = %f\n",s->player.angle, fake_angle);
+		// 	fake_vs = ft_place_view_plan(s, fake_player, fake_angle, 0x4bd9ffff);
+		// 	fake_vs.sct_id = vtx->sct_dest;
+		// 	demi_fov = ft_find_angle_plan(ft_dist_t_dpos(fake_player, fake_vs.left_plan), METRE, WIDTHPLAN / 2);
+		// 	// printf("demi_fov = %f\n",demi_fov);
+		// 	angle_left = fake_angle + demi_fov;
+		// 	angle_left = angle_left > 360 ? angle_left - 360 : angle_left;
+		// 	angle_right = fake_angle - demi_fov;
+		// 	angle_right = angle_right < 0 ? angle_right + 360: angle_right;
+		// 	fake_vs = get_walls_to_draw(s, fake_player, angle_left, angle_right, fake_vs);
+		// 	fake_vs.begin_wall_id = vtx->vtx_dest->ptr->id;
+		// 	fake_vs.end_wall_id = vtx->vtx_dest->prev->ptr->id;
+		// 	if (fake_player.x > -10000 && fake_player.x < 1000 && fake_player.y > -10000 && fake_player.y < 1000)
+		// 		add_portal_to_list(s, fake_player, get_sector_by_id(s, vtx->sct_dest), fake_vs);
+		// 	vtx = vtx_ori->next;
+		// }
+		// //
+		// else
+		// 	ft_create_new_wall(s, vtx, vs);
 		vtx = vtx->next;
 		// {s->line.x1 = wall->l_plan.x + s->editor->decal_x;
 		// 	s->line.y1 = wall->l_plan.y + s->editor->decal_y;
--- src/game/visu_portal.c
+++ src/game/visu_portal.c
@@ -45,12 +45,12 @@ double		ft_find_angle_portal(t_dpos *left, t_dpos *right, t_dpos *third, int nee
 	return (angle);
 }
 
-t_visu		ft_get_fake_vs(t_main *s, t_dpos fake_player, double fake_angle)
-{
-	t_visu	fake_vs;
-
-	fake_vs = ft_place_view_plan(s, fake_player, fake_angle, 0x4bd9ffff);
-}
+// t_visu		ft_get_fake_vs(t_main *s, t_dpos fake_player, double fake_angle)
+// {
+// 	t_visu	fake_vs;
+//
+// 	fake_vs = ft_place_view_plan(s, fake_player, fake_angle, 0x4bd9ffff);
+// }
 
 double		ft_get_fake_angle(t_main *s, t_dpos player, t_int *vtx)
 {
@@ -97,12 +97,55 @@ double		ft_get_fake_angle(t_main *s, t_dpos player, t_int *vtx)
 	// modifier angle et fake player
 }
 
-t_dpos		ft_get_fake_player(t_main *s, t_dpos player, double angle_fake_player, t_int *vtx)
+void		add_portal_to_list(t_main *s, t_dpos player, t_sector *sct, t_visu vs)
+{
+	t_walls		*tmp;
+	t_dpos		plan_left;
+	int			x;
+	t_int		*vtx;
+	int			new_x;
+
+	x = 0;
+	new_x = 0;
+	// printf("sct->vertex")
+	vtx = sct->vertex;
+	printf("vs.begin_wall_id = %d\n", vs.begin_wall_id);
+	printf("vs.end_wall_id = %d\n", vs.end_wall_id);
+	//On recuper el'angle entre le joueur et le point de gauche, ca donne le mur de gauche.
+	//On recuper el'angle entre le joueur et le point de droite, ca donne le mur de droite.
+	vtx = get_t_int_by_vertex_id(vtx, vs.begin_wall_id);
+	draw_first_wall(s, vtx, &vs);
+
+	plan_left = s->tmp_intersect;
+	// printf("end wall id = %d\n", get_t_int_by_vertex_id(sct->vertex, vs.end_wall_id)->id);
+	if (vs.begin_wall_id == vs.end_wall_id) // cas 1 seul mur
+	{
+		tmp = s->walls;
+		ft_print_wall(s, tmp->x, player, tmp->left, tmp->right, tmp->l_plan, tmp->r_plan);
+		return ;
+	}
+
+	vtx = vtx->next;
+	vtx = draw_mid_walls(s, vtx, &vs);
+
+	draw_last_wall(vtx, &vs);
+	ft_find_intersection(s, vs.begin, vs.player, vs.left_plan, vs.right_plan, 1);
+	// plan_left = s->tmp_intersect;
+	// x = (ft_dist_t_dpos(vs.left_plan, plan_left) / WIDTHPLAN) * WIDTH;
+	ft_create_new_wall(s, vtx, &vs);
+
+	// print_wall_list(s);
+}
+
+t_dpos		ft_get_fake_player(t_main *s, t_dpos player, t_int *vtx, double *angle_fake)
 {
 	(void)s;
 	t_dpos		l_portal;
 	t_dpos		r_portal;
 	t_dpos		fake_player;
+	double		fake_angle;
+	double		angle_portal_in;
+	double		angle_portal_out;
 	t_pos		fake_player2; //juste pour affichage du faek player
 	double		dist_player;
 	t_dpos		lwall;
@@ -122,10 +165,30 @@ t_dpos		ft_get_fake_player(t_main *s, t_dpos player, double angle_fake_player, t
 	//trouver la distance entre joueur et le point gauche du portail initial
 	dist_player = ft_dist_t_dpos(player, lwall);
 
+	fake_angle = ft_find_angle_portal(&lwall, &rwall, &player, 1);
+
+	//trouver langle du portail d'entree
+	angle_portal_in = ft_find_angle_portal(&lwall, &rwall, NULL, 1);
+	if (rwall.y > lwall.y)
+		angle_portal_in = 180 + (180 - angle_portal_in);
+
+	//trouver langle du portail de sortie
+	angle_portal_out = ft_find_angle_portal(&l_portal, &r_portal, NULL, 1);
+	if (r_portal.y > l_portal.y)
+		angle_portal_out = 180 + (180 - angle_portal_out);
+
+
+	// printf("angle PLAYER  (%f)\n",angle_fake_player);
+	printf("angle IN      (%f)\n",angle_portal_in);
+	printf("angle OUT     (%f)\n\n",angle_portal_out);
+	*angle_fake = s->player.angle + (angle_portal_in - angle_portal_out);
+
+	//application de la difference d'angle des deux portail sur angle player
+	fake_angle = angle_portal_in - fake_angle - fabs(angle_portal_in - angle_portal_out);
 	//Tentative de placement du fake player
-	fake_player.x = r_portal.x + cos(to_rad(angle_fake_player)) * dist_player;
-	fake_player.y = r_portal.y - sin(to_rad(angle_fake_player)) * dist_player;
-//	printf("\n\nPOSITION DU FAKE PLAYER (%.1f, %.1f)\n", fake_player.x, fake_player.y);
+	fake_player.x = r_portal.x + cos(to_rad(fake_angle)) * dist_player;
+	fake_player.y = r_portal.y - sin(to_rad(fake_angle)) * dist_player;
+	printf("\n\nPOSITION DU FAKE PLAYER (%.1f, %.1f)\n", fake_player.x, fake_player.y);
 
 	//ATTENTION !!!! jai ajouter le decal_x pour que le point saffiche a lecran pour mes tests.
 
