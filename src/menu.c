accab7235189a760fc0814ab2913b9e9e7fb2f41
JoanDeulofeu <jgehin@student.42.fr>
2020-02-04 18:21:31 +0100

    menu.c norme ✅

--- src/menu.c
+++ src/menu.c
@@ -2,14 +2,14 @@
 
 int		play_intro(t_main *s)
 {
-	t_pos 	coord;
+	t_pos	coord;
 	int		key;
 
-	coord.x = 0;
-	coord.y = 0;
+	ft_bzero((void*)&coord, sizeof(t_pos));
 	while (s->cinematic.current < 4)
 	{
-		draw_plain_sprite(s, coord, s->cinematic.image[s->cinematic.current], s->sdl->game);
+		draw_plain_sprite(s, coord, s->cinematic.image[s->cinematic.current],
+			s->sdl->game);
 		update_image(s, s->sdl->game);
 		SDL_WaitEvent(&s->sdl->event);
 		if (s->sdl->event.type == SDL_QUIT)
@@ -23,19 +23,16 @@ int		play_intro(t_main *s)
 			else if (key == SDLK_ESCAPE)
 				return (1);
 		}
-		else
-			continue;
 	}
 	s->cinematic.current = 0;
 	return (1);
 }
 
-void	display_menu(t_main *s, int i, int j)
+void	display_menu(t_main *s, int i, int j, int px)
 {
 	double		perx;
 	double		pery;
 	t_pos		coord;
-	int			px;
 
 	coord.x = 0;
 	coord.y = 0;
@@ -44,14 +41,16 @@ void	display_menu(t_main *s, int i, int j)
 		j = 0;
 		coord.x = i;
 		perx = (double)coord.x / (double)WIDTH;
-		while (j < HEIGHT)
+		while ((coord.y = j++) < HEIGHT)
 		{
-			coord.y = j++;
 			pery = (double)coord.y / (double)HEIGHT;
-			px = (int)(pery * (double)s->menu.image[s->menu.current]->h) * s->menu.image[s->menu.current]->w + (int)
+			px = (int)(pery * (double)s->menu.image[s->menu.current]->h)
+			* s->menu.image[s->menu.current]->w + (int)
 			(perx * (double)s->menu.image[s->menu.current]->w);
-			if (px >= 0 && px < s->menu.image[s->menu.current]->w * s->menu.image[s->menu.current]->h)
-				set_pixel(s->sdl->game, s->menu.image[s->menu.current]->tex[px], coord);
+			if (px >= 0 && px < s->menu.image[s->menu.current]->w
+				* s->menu.image[s->menu.current]->h)
+				set_pixel(s->sdl->game,
+					s->menu.image[s->menu.current]->tex[px], coord);
 		}
 		i++;
 	}
@@ -75,18 +74,13 @@ void	select_game_mode(t_main *s, int key)
 			s->menu.current = 1;
 	}
 	Mix_PlayChannel(2, s->sdl->sounds.select, 0);
-	if(s->sdl->sounds.select == NULL)
-		printf("true\n");
-	display_menu(s, 0, 0);
+	display_menu(s, 0, 0, 0);
 }
 
-int		handle_menu(t_main *s)
+int		handle_menu2(t_main *s)
 {
 	int key;
-	play_music(s);
-	reset(s);
-	s->play_or_editor = 2;
-	display_menu(s, 0, 0);
+
 	while (1)
 	{
 		SDL_WaitEvent(&s->sdl->event);
@@ -98,16 +92,25 @@ int		handle_menu(t_main *s)
 			if (key == SDLK_RETURN
 				|| s->sdl->event.key.keysym.sym == SDLK_KP_ENTER)
 				break ;
-			else if(key == SDLK_a || key == SDLK_d)
+			else if (key == SDLK_a || key == SDLK_d)
 			{
 				select_game_mode(s, key);
 			}
 			else if (key == SDLK_ESCAPE)
 				return (0);
 		}
-		else
-			continue;
 	}
+	return (1);
+}
+
+int		handle_menu(t_main *s)
+{
+	play_music(s);
+	reset(s);
+	s->play_or_editor = 2;
+	display_menu(s, 0, 0, 0);
+	if (handle_menu2(s) == 0)
+		return (0);
 	if (s->sdl->musique != NULL)
 	{
 		Mix_HaltMusic();
