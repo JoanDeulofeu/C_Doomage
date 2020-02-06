3dfddc139d0e87ca39c85b3197d17e3de42c3236
Ydonse <ydonse@student.42.fr>
2020-02-06 12:38:23 +0100

    wp_anims.c norme ✅

--- src/launching/load_wp_anim.c
+++ src/launching/load_wp_anim.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_wp_anim.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydonse <ydonse@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/06 12:28:16 by ydonse            #+#    #+#             */
+/*   Updated: 2020/02/06 12:38:02 by ydonse           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "doom.h"
 
 t_anim				load_gun_anim(t_anim data)
@@ -13,8 +25,10 @@ t_anim				load_shotgun_anim(t_anim data)
 {
 	ft_bzero((void *)&data, sizeof(t_anim));
 	data.image[0] = load_tga("images/weapons/shotgun/shotgun1.tga", 0, 0, 0);
-	data.image[1] = load_tga("images/weapons/shotgun/shotgun_fire.tga", 0, 0, 0);
-	data.image[2] = load_tga("images/weapons/shotgun/shotgun_fire.tga", 0, 0, 0);
+	data.image[1] = load_tga("images/weapons/shotgun/shotgun_fire.tga",
+			0, 0, 0);
+	data.image[2] = load_tga("images/weapons/shotgun/shotgun_fire.tga",
+			0, 0, 0);
 	data.image[3] = load_tga("images/weapons/shotgun/shotgun2.tga", 0, 0, 0);
 	data.image[4] = load_tga("images/weapons/shotgun/shotgun3.tga", 0, 0, 0);
 	data.image[5] = load_tga("images/weapons/shotgun/shotgun4.tga", 0, 0, 0);
@@ -32,7 +46,7 @@ t_anim				load_kick_anim(t_anim data)
 	return (data);
 }
 
-t_anim_wp		load_wp_anims(t_main *s)
+t_anim_wp			load_wp_anims(t_main *s)
 {
 	ft_bzero((void *)&s->wp_anims, sizeof(t_anim_wp));
 	s->wp_anims.gun = load_gun_anim(s->wp_anims.gun);
