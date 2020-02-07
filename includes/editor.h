ca1eb9c84fddcb02f9ddca80ecfc4bc6ad9275c5
JoanDeulofeu <jgehin@student.42.fr>
2020-02-07 20:16:36 +0100

    editor.h norme ✅

--- includes/editor.h
+++ includes/editor.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   editor.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgehin <jgehin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/07 20:13:51 by jgehin            #+#    #+#             */
+/*   Updated: 2020/02/07 20:16:12 by jgehin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef EDITOR_H
 # define EDITOR_H
 # include "libft.h"
@@ -19,12 +31,10 @@
 # define GRID_TOP_MARGIN 20
 # define ROTATE_SPEED 20.00
 # define PARSE_BUFF_SIZE 64
-
 # define INT_MAX 2147480000
 # define INT_MIN -2147480000
 # define LONG_MAX 9223372036854775807
 # define DEF_TEX "images/textures/02.tga"
-
 # define FILE_ERROR 1
 # define SIZE_ERROR 2
 # define SYNTAX_ERROR 3
@@ -34,19 +44,16 @@
 # define POINTER_ERROR 7
 # define SECTOR_ERROR 8
 # define MAP_ERROR 9
-
 # define LEFT_AR		SDL_SCANCODE_LEFT
 # define RIGHT_AR		SDL_SCANCODE_RIGHT
 # define UP_AR			SDL_SCANCODE_UP
 # define DOWN_AR		SDL_SCANCODE_DOWN
-
 # define SPACE			32
 # define LCTRL			SDL_SCANCODE_LCTRL
 # define RCTRL			SDL_SCANCODE_RCTRL
 # define SPRINT			SDL_SCANCODE_LSHIFT
 # define PAGE_UP		1073741899
 # define PAGE_DOWN		1073741902
-
 # define LEFT			SDL_SCANCODE_A
 # define RIGHT			SDL_SCANCODE_D
 # define UP				SDL_SCANCODE_W
@@ -76,10 +83,7 @@
 # define END			1073741901
 # define PAGE_UP		1073741899
 # define PAGE_DOWN		1073741902
-
-
 # define SPRINT	SDL_SCANCODE_LSHIFT
-
 # define YELLOW 0xFFF73611
 # define PINK 0xd05ddaff
 # define WHITE 0xFFFFFFFF
@@ -88,7 +92,6 @@
 # define BLUE 0x57C7FFFF
 # define BLACK_SCREEN 0x13131dFF
 # define COLOR_WALL 0xff7062FF
-
 # define S_YELLOW 0xFFF73611
 # define S_PINK 0xFF36F7FF
 # define S_BLUE 0x57C7FFFF
@@ -101,14 +104,13 @@
 # define S_DGREEN 0x288333FF
 # define APPLE_GREEN 0xa5d65dff
 
-
-typedef enum			e_type {
+typedef enum				e_type {
 	enemy,
 	obstacle,
 	item
-}						t_type;
+}							t_type;
 
-typedef enum			e_name {
+typedef enum				e_name {
 	none,
 	storm,
 	table,
@@ -119,9 +121,9 @@ typedef enum			e_name {
 	jetpack,
 	big_g_ammo,
 	big_s_ammo
-}						t_name;
+}							t_name;
 
-typedef enum			e_mode {
+typedef enum				e_mode {
 	move,
 	sector,
 	vertex,
@@ -129,84 +131,84 @@ typedef enum			e_mode {
 	save_edi,
 	portal,
 	sprite
-}						t_mode;
+}							t_mode;
 
-typedef struct			s_abpos {
-	float				a;
-	float				b;
-}						t_abpos;
+typedef struct				s_abpos {
+	float					a;
+	float					b;
+}							t_abpos;
 
-typedef struct			s_dpos {
-	double				x;
-	double				y;
-}						t_dpos;
+typedef struct				s_dpos {
+	double					x;
+	double					y;
+}							t_dpos;
 
-typedef struct			s_4double {
-	double				d1;
-	double				d2;
-	double				d3;
-	double				d4;
-}						t_4double;
+typedef struct				s_4double {
+	double					d1;
+	double					d2;
+	double					d3;
+	double					d4;
+}							t_4double;
 
-typedef struct			s_line {
-	int					x1;
-	int					x2;
-	int					y1;
-	int					y2;
-	int					e;
-	int					pixel_o;
-	int					dx;
-	int					dy;
-}						t_line;
+typedef struct				s_line {
+	int						x1;
+	int						x2;
+	int						y1;
+	int						y2;
+	int						e;
+	int						pixel_o;
+	int						dx;
+	int						dy;
+}							t_line;
 
-typedef struct			s_mouse {
-	double				x;
-	double				y;
-}						t_mouse;
+typedef struct				s_mouse {
+	double					x;
+	double					y;
+}							t_mouse;
 
-typedef struct			s_point {
-	short				x;
-	short				y;
-	char				anchor;
-	char				clicked;
-}						t_point;
+typedef struct				s_point {
+	short					x;
+	short					y;
+	char					anchor;
+	char					clicked;
+}							t_point;
 
-typedef struct			s_image {
-	int					bits_color;
-	int					bits_alpha;
-	Uint32				*tex;
-	int					w;
-	int					h;
-}						t_image;
+typedef struct				s_image {
+	int						bits_color;
+	int						bits_alpha;
+	Uint32					*tex;
+	int						w;
+	int						h;
+}							t_image;
 
-typedef struct			s_anim {
-	int					current;
-	t_image				*image[19];
-}						t_anim;
+typedef struct				s_anim {
+	int						current;
+	t_image					*image[19];
+}							t_anim;
 
-typedef struct			s_pos {
-	int					x;
-	int					y;
-}						t_pos;
+typedef struct				s_pos {
+	int						x;
+	int						y;
+}							t_pos;
 
-typedef struct			s_element {
-	t_pos				pixel;
-	struct s_element	*next;
-}						t_element;
+typedef struct				s_element {
+	t_pos					pixel;
+	struct s_element		*next;
+}							t_element;
 
-typedef struct			s_vertex {
-	int					id;
-	int					x;
-	int					y;
-	t_pos				pos; // position en pixel en rapport a l'editeur
-	t_dpos				m_pos; // x et y * METRE
-	t_pos				old;
-	t_pos				save_pos;
-	int					selec;
-	char				selected;
-	struct s_vertex		*next;
-	struct s_vertex		*prev;
-}						t_vertex;
+typedef struct				s_vertex {
+	int						id;
+	int						x;
+	int						y;
+	t_pos					pos;
+	t_dpos					m_pos;
+	t_pos					old;
+	t_pos					save_pos;
+	int						selec;
+	char					selected;
+	struct s_vertex			*next;
+	struct s_vertex			*prev;
+}							t_vertex;
 
 typedef struct				s_int {
 	int						id;
@@ -266,7 +268,7 @@ typedef struct				s_sprite
 	int						x;
 	int						set;
 	int						displayed;
-	int						inverse; //savoir si on inverse le display sprite
+	int						inverse;
 	int						anim_ms;
 	int						shoot_ms;
 	int						orientation;
@@ -277,16 +279,16 @@ typedef struct				s_sprite
 	int						destroy;
 	int						damage;
 	double					angle;
-	double					s_angle; //direction du regard du sprite
+	double					s_angle;
 	int						dist;
 	double					r_dist;
 	t_dpos					r_pos;
-	t_dpos					r_ori; //Position a sauvegarder
+	t_dpos					r_ori;
 	t_pos					pos;
 	t_dpos					m_pos;
 	double					l_dist;
-	t_type					type; //type a sauvegarder
-	t_name					name; //name a sauvegarder
+	t_type					type;
+	t_name					name;
 	t_anim_name				a_name;
 	t_image					*img;
 	t_anim					anim;
@@ -302,43 +304,43 @@ typedef struct				s_lsprite
 	struct s_lsprite		*next;
 }							t_lsprite;
 
-typedef struct			s_sector {
-	int					id;
-	int					floor;
-	int					ceiling;
-	t_lsprite			*liste;
-	t_int				*vertex;
-	struct s_sector		*next;
-	struct s_sector		*prev;
-}						t_sector;
+typedef struct				s_sector {
+	int						id;
+	int						floor;
+	int						ceiling;
+	t_lsprite				*liste;
+	t_int					*vertex;
+	struct s_sector			*next;
+	struct s_sector			*prev;
+}							t_sector;
 
-typedef struct			s_editor {
-	t_line				line;
-	int					selected;
-	int					space;
-	int					anchor_size;
-	int					decal_x;
-	int					decal_y;
-	t_pos				ref;
-	t_mode				mode;
-	char				color_sector;
-	int					dply_floor;
-	char				mode_floor;
-	t_anim				menu;
-	t_name				sprite_selected;
-	t_anim				sprite_menu;
-	t_type				sprite_type;
-	t_anim				all_sprite;
-	t_anim				all_texture;
-	t_pos				m_sprite_pos[10];
-	int					select_sprite;
-	t_anim				m_floor;
-	t_int				*wall;
-	t_int				*wall2;
-	char				portal_temp;
-	int					over_portal;
-	int					over_sector;
-	int					over_sector2;
-}						t_editor;
+typedef struct				s_editor {
+	t_line					line;
+	int						selected;
+	int						space;
+	int						anchor_size;
+	int						decal_x;
+	int						decal_y;
+	t_pos					ref;
+	t_mode					mode;
+	char					color_sector;
+	int						dply_floor;
+	char					mode_floor;
+	t_anim					menu;
+	t_name					sprite_selected;
+	t_anim					sprite_menu;
+	t_type					sprite_type;
+	t_anim					all_sprite;
+	t_anim					all_texture;
+	t_pos					m_sprite_pos[10];
+	int						select_sprite;
+	t_anim					m_floor;
+	t_int					*wall;
+	t_int					*wall2;
+	char					portal_temp;
+	int						over_portal;
+	int						over_sector;
+	int						over_sector2;
+}							t_editor;
 
 #endif
